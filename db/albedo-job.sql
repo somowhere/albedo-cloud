f8585f5c7d225b850fd79f84e4681ac3cd205406
somewhere <somewhere0813@gmail.coom>
2021-02-27 08:49:07 +0800

    Regular upgrade[3.0.5-SNAPSHOT]

--- db/albedo-job.sql
+++ db/albedo-job.sql
@@ -11,16 +11,12 @@
  Target Server Version : 80022
  File Encoding         : 65001
 
- Date: 26/12/2020 11:33:34
+ Date: 27/02/2021 08:48:23
 */
-DROP DATABASE IF EXISTS `albedo-job`;
-
-CREATE DATABASE  `albedo-job` DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci;
 
 SET NAMES utf8mb4;
 SET FOREIGN_KEY_CHECKS = 0;
 
-USE `albedo-job`;
 -- ----------------------------
 -- Table structure for xxl_job_group
 -- ----------------------------
@@ -30,15 +26,16 @@ CREATE TABLE `xxl_job_group` (
   `app_name` varchar(64) NOT NULL COMMENT '执行器AppName',
   `title` varchar(12) NOT NULL COMMENT '执行器名称',
   `address_type` tinyint NOT NULL DEFAULT '0' COMMENT '执行器地址类型：0=自动注册、1=手动录入',
-  `address_list` varchar(512) DEFAULT NULL COMMENT '执行器地址列表，多地址逗号分隔',
+  `address_list` text COMMENT '执行器地址列表，多地址逗号分隔',
+  `update_time` datetime DEFAULT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
 -- ----------------------------
 -- Records of xxl_job_group
 -- ----------------------------
 BEGIN;
-INSERT INTO `xxl_job_group` VALUES (1, 'xxl-job-executor-sample', '示例执行器', 0, NULL);
+INSERT INTO `xxl_job_group` VALUES (1, 'xxl-job-executor-sample', '示例执行器', 0, NULL, '2021-02-27 08:48:18');
 COMMIT;
 
 -- ----------------------------
@@ -48,12 +45,14 @@ DROP TABLE IF EXISTS `xxl_job_info`;
 CREATE TABLE `xxl_job_info` (
   `id` int NOT NULL AUTO_INCREMENT,
   `job_group` int NOT NULL COMMENT '执行器主键ID',
-  `job_cron` varchar(128) NOT NULL COMMENT '任务执行CRON',
   `job_desc` varchar(255) NOT NULL,
   `add_time` datetime DEFAULT NULL,
   `update_time` datetime DEFAULT NULL,
   `author` varchar(64) DEFAULT NULL COMMENT '作者',
   `alarm_email` varchar(255) DEFAULT NULL COMMENT '报警邮件',
+  `schedule_type` varchar(50) NOT NULL DEFAULT 'NONE' COMMENT '调度类型',
+  `schedule_conf` varchar(128) DEFAULT NULL COMMENT '调度配置，值含义取决于调度类型',
+  `misfire_strategy` varchar(50) NOT NULL DEFAULT 'DO_NOTHING' COMMENT '调度过期策略',
   `executor_route_strategy` varchar(50) DEFAULT NULL COMMENT '执行器路由策略',
   `executor_handler` varchar(255) DEFAULT NULL COMMENT '执行器任务handler',
   `executor_param` varchar(512) DEFAULT NULL COMMENT '执行器任务参数',
@@ -69,13 +68,13 @@ CREATE TABLE `xxl_job_info` (
   `trigger_last_time` bigint NOT NULL DEFAULT '0' COMMENT '上次调度时间',
   `trigger_next_time` bigint NOT NULL DEFAULT '0' COMMENT '下次调度时间',
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
 -- ----------------------------
 -- Records of xxl_job_info
 -- ----------------------------
 BEGIN;
-INSERT INTO `xxl_job_info` VALUES (1, 1, '0 0 0 * * ? *', '测试任务1', '2018-11-03 22:21:31', '2020-04-15 13:58:35', 'somewhere', '', 'ROUND', 'demoJobHandler', '', 'DISCARD_LATER', 0, 0, 'BEAN', '', 'GLUE代码初始化', '2018-11-03 22:21:31', '', 0, 0, 0);
+INSERT INTO `xxl_job_info` VALUES (1, 1, '测试任务1', '2018-11-03 22:21:31', '2018-11-03 22:21:31', 'XXL', '', 'CRON', '0 0 0 * * ? *', 'DO_NOTHING', 'FIRST', 'demoJobHandler', '', 'SERIAL_EXECUTION', 0, 0, 'BEAN', '', 'GLUE代码初始化', '2018-11-03 22:21:31', '', 0, 0, 0);
 COMMIT;
 
 -- ----------------------------
@@ -85,7 +84,7 @@ DROP TABLE IF EXISTS `xxl_job_lock`;
 CREATE TABLE `xxl_job_lock` (
   `lock_name` varchar(50) NOT NULL COMMENT '锁名称',
   PRIMARY KEY (`lock_name`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
 -- ----------------------------
 -- Records of xxl_job_lock
@@ -117,7 +116,7 @@ CREATE TABLE `xxl_job_log` (
   PRIMARY KEY (`id`),
   KEY `I_trigger_time` (`trigger_time`),
   KEY `I_handle_code` (`handle_code`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
 -- ----------------------------
 -- Records of xxl_job_log
@@ -135,22 +134,18 @@ CREATE TABLE `xxl_job_log_report` (
   `running_count` int NOT NULL DEFAULT '0' COMMENT '运行中-日志数量',
   `suc_count` int NOT NULL DEFAULT '0' COMMENT '执行成功-日志数量',
   `fail_count` int NOT NULL DEFAULT '0' COMMENT '执行失败-日志数量',
+  `update_time` datetime DEFAULT NULL,
   PRIMARY KEY (`id`),
   UNIQUE KEY `i_trigger_day` (`trigger_day`) USING BTREE
-) ENGINE=InnoDB AUTO_INCREMENT=9 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
 -- ----------------------------
 -- Records of xxl_job_log_report
 -- ----------------------------
 BEGIN;
-INSERT INTO `xxl_job_log_report` VALUES (1, '2020-12-20 00:00:00', 0, 0, 0);
-INSERT INTO `xxl_job_log_report` VALUES (2, '2020-12-19 00:00:00', 0, 0, 0);
-INSERT INTO `xxl_job_log_report` VALUES (3, '2020-12-18 00:00:00', 0, 0, 0);
-INSERT INTO `xxl_job_log_report` VALUES (4, '2020-12-24 00:00:00', 0, 0, 0);
-INSERT INTO `xxl_job_log_report` VALUES (5, '2020-12-23 00:00:00', 0, 0, 0);
-INSERT INTO `xxl_job_log_report` VALUES (6, '2020-12-22 00:00:00', 0, 0, 0);
-INSERT INTO `xxl_job_log_report` VALUES (7, '2020-12-25 00:00:00', 0, 0, 0);
-INSERT INTO `xxl_job_log_report` VALUES (8, '2020-12-26 00:00:00', 0, 0, 0);
+INSERT INTO `xxl_job_log_report` VALUES (1, '2021-02-27 00:00:00', 0, 0, 0, NULL);
+INSERT INTO `xxl_job_log_report` VALUES (2, '2021-02-26 00:00:00', 0, 0, 0, NULL);
+INSERT INTO `xxl_job_log_report` VALUES (3, '2021-02-25 00:00:00', 0, 0, 0, NULL);
 COMMIT;
 
 -- ----------------------------
@@ -166,7 +161,7 @@ CREATE TABLE `xxl_job_logglue` (
   `add_time` datetime DEFAULT NULL,
   `update_time` datetime DEFAULT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
 -- ----------------------------
 -- Records of xxl_job_logglue
@@ -186,7 +181,7 @@ CREATE TABLE `xxl_job_registry` (
   `update_time` datetime DEFAULT NULL,
   PRIMARY KEY (`id`),
   KEY `i_g_k_v` (`registry_group`,`registry_key`,`registry_value`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
 -- ----------------------------
 -- Records of xxl_job_registry
@@ -206,7 +201,7 @@ CREATE TABLE `xxl_job_user` (
   `permission` varchar(255) DEFAULT NULL COMMENT '权限：执行器ID列表，多个逗号分割',
   PRIMARY KEY (`id`),
   UNIQUE KEY `i_username` (`username`) USING BTREE
-) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
 -- ----------------------------
 -- Records of xxl_job_user
